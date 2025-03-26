b43b3c000943fcfa5cf270166c97d1c7646d3b97
acdpsn <akim8@emich.edu>
2025-03-26 15:36:42 -0400

    add password hashing

--- app.ts
+++ app.ts
@@ -1,15 +1,20 @@
 import express, { type Request, type Response } from "express";
 import pgPromise from "pg-promise";
+import argon2 from "argon2";
 
 const app = express();
+
 const port = process.env?.PORT;
+const pgUsername = process.env?.PG_USERNAME;
+const pgPassword = process.env?.PG_PASSWORD;
+const pgHost = process.env?.PG_HOST;
+const pgPort = process.env?.PG_PORT;
 
 const initOptions = {
 	schema: "user_info",
 };
 const pgp = pgPromise(initOptions);
-// const db = pgp('postgres://username:password@host:port/database')
-const db = pgp("postgres://bookstore:next@localhost:5432/");
+const db = pgp(`postgres://${pgUsername}:${pgPassword}@${pgHost}:${pgPort}/`);
 
 app.use(express.json());
 
@@ -30,33 +35,40 @@ interface NewUser {
 	uuid: string;
 	username: string;
 	hash: string;
-	salt: string;
 	display_name?: string;
 	is_admin: boolean;
 	account_status: string;
 	referrer?: string;
 }
 
-app.put("/create-user", (req: CreateUserRequestProps, res: Response) => {
-	const data: NewUser = {
-		uuid: crypto.randomUUID(),
-		username: req.body.username,
-		hash: "hash_val", // todo
-		salt: "salt_val", // todo
-		display_name: req.body?.display_name,
-		is_admin: false,
-		account_status: "user",
-		referrer: req.body?.referrer,
-	};
-
-	const insert =
-		"INSERT INTO users(uuid, username, hash, salt, display_name, is_admin, account_status, referrer) VALUES($1, $2, $3, $4, $5, $6, $7, $8)";
-
-	db.none(insert, Object.values(data))
-		.then(() => {
-			res.send(`inserted user ${data.username}`);
-		})
-		.catch((error: unknown) => {
-			res.status(400).send(error);
+app.put("/create-user", async (req: CreateUserRequestProps, res: Response) => {
+	try {
+		const hash = await argon2.hash(req.body.password, {
+			memoryCost: 19456,
+			timeCost: 2,
+			parallelism: 1,
 		});
+
+		const data: NewUser = {
+			uuid: crypto.randomUUID(),
+			username: req.body.username,
+			hash,
+			display_name: req.body?.display_name,
+			is_admin: false,
+			account_status: "user",
+			referrer: req.body?.referrer,
+		};
+
+		const insert =
+			"INSERT INTO users(uuid, username, hash, display_name, is_admin, account_status, referrer) VALUES($1, $2, $3, $4, $5, $6, $7)";
+
+		try {
+			db.none(insert, Object.values(data));
+			res.send(`inserted user ${data.username}`);
+		} catch (error) {
+			res.status(400).send(error);
+		}
+	} catch (error) {
+		res.status(400).send(error);
+	}
 });
--- sql_scripts/users.sql
+++ sql_scripts/users.sql
@@ -1,3 +1,6 @@
+CREATE SCHEMA
+	user_info
+
 CREATE TABLE
   user_info.users (
     id serial NOT NULL,
@@ -5,10 +8,10 @@ CREATE TABLE
     uuid uuid NOT NULL,
     username text NOT NULL,
     hash text NOT NULL,
-    salt text NOT NULL,
     display_name text NULL,
     is_admin boolean NOT NULL,
     account_status text NOT NULL,
+		last_login timestamp without time zone NULL,
     referrer uuid NULL,
     can_generate_referral_codes boolean NULL
   );
