ac95f6c24abad611050d36705da2cd27433c4133
acdpsn <akim8@emich.edu>
2025-04-07 20:31:03 -0400

    set jwt cookie on login

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 import create from "./routes/user/create.ts";
-import authorize from "./routes/user/authorize.ts";
+import login from "./routes/user/login.ts";
 
 const port = process.env?.PORT;
 
@@ -9,7 +9,11 @@ const app = express();
 
 app.use(express.json());
 app.use("/user", create);
-app.use("/user", authorize);
+app.use("/user", login);
+app.use((req, res, next) => {
+	res.setHeader("Content-Type", "application/json");
+	res.status(404).send("Not Found");
+});
 
 app.listen(port, () => {
 	console.log(`running at http://localhost:${port}`);
--- routes/user/login.ts
+++ routes/user/login.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import argon2 from "argon2";
+import * as jose from "jose";
 
 import { db } from "./db.ts";
 
 const router = express.Router();
+const msInMin = 60000;
+const jwtAge = (Number(process.env?.JWT_EXP) || 120) * msInMin;
 
 router
-	.route("/authorize")
+	.route("/login")
 	.get(async (req: express.Request, res: express.Response) => {
 		try {
 			const hashes = await db.any(
@@ -16,6 +19,8 @@ router
 
 			try {
 				if (await argon2.verify(hashes[0].hash, req.body.password)) {
+					const jwt = await getJwt(req.body.username);
+					res.cookie("token", jwt, { maxAge: jwtAge });
 					res.send("match");
 				} else {
 					res.status(400).send("mismatch");
@@ -27,5 +32,20 @@ router
 			res.status(500).send(err);
 		}
 	});
+
+const getJwt = async (username: string) => {
+	if (typeof process.env.JWT_SECRET === "undefined") {
+		throw new Error("JWT_SECRET is undefined");
+	}
+
+	const secret = jose.base64url.decode(process.env.JWT_SECRET);
+
+	const jwt = await new jose.SignJWT({ username })
+		.setProtectedHeader({ alg: "HS256" })
+		.setExpirationTime(jwtAge)
+		.sign(secret);
+
+	return jwt;
+};
 
 export default router;
